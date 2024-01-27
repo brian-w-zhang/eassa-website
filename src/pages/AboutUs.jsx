96b60b164e658a7a3ab451293a51cec4c43603b8
Brian Zhang <brian.w.zhang@gmail.com>
2024-01-26 23:59:21 -0500

    adjust about us image

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,17 @@
+import dragon_bg from '../landscapes/dragon background.jpg';
+import landscape1 from '../landscapes/landscape 1.jpg';
+
 function AboutUs() {
   return (
     <>
-      <div className="placeholder">
-        <h1>About Us</h1>
+      <div className="dragon-bg-container">
+        <img className="dragon-background" src={dragon_bg} />
+      </div>
+      <div className="white-overlay"></div>
+
+      <div className="about-us-container">
+        <img src={landscape1} className="pages-image" alt="landscape" />
+        <h1 className="about-us-title">About Us</h1>
       </div>
     </>
   );
