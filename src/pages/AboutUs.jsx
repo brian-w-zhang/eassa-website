fd2f9495dd51852797b14c55272bae02a6cdba5a
Brian Zhang <brian.w.zhang@gmail.com>
2024-02-06 11:48:56 -0500

    add text to about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,43 @@
 import dragon_bg from '../landscapes/dragon background.jpg';
 import landscape1 from '../landscapes/landscape 1.jpg';
 import line from '../assets/linecool.png';
+import { useEffect, useRef } from 'react';
 
 function AboutUs() {
+  const aboutSectionRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const aboutSection = aboutSectionRef.current;
+      if (isElementInViewport(aboutSection, 0.7)) {
+        aboutSection.classList.add('animated');
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const isElementInViewport = (element, threshold = 1) => {
+    if (!element) return false;
+
+    const rect = element.getBoundingClientRect();
+    const windowHeight =
+      window.innerHeight || document.documentElement.clientHeight;
+
+    return (
+      rect.top < windowHeight * threshold &&
+      rect.bottom >= 0 &&
+      rect.right >= 0 &&
+      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
   return (
     <>
       <div className="dragon-bg-container">
@@ -20,7 +55,21 @@ function AboutUs() {
         <img className="border-line" src={line} alt="border line" />
       </div>
 
-      <div className="page-content"></div>
+      <div className="about-us-container">
+        <div className="about-text-section" ref={aboutSectionRef}>
+          <p className="who-we-are-text">Who We Are</p>
+          <p className="about-main-text">
+            We&apos;re a team of undergraduate East Asian Studies students who
+            work together on various projects with the goal of bringing together
+            and promoting the EAS department. We strive to provide EAS students
+            with the opportunities they deserve, share information from outside
+            the McGill bubble to expand horizons, and create a welcoming and
+            inspiring environment for the EAS student body to thrive. We&apos;re
+            always welcoming of feedback on our work, and wish for people to
+            feel welcome with being active within the department!
+          </p>
+        </div>
+      </div>
 
       <div className="border-line-container">
         <img className="border-line" src={line} alt="border line" />
